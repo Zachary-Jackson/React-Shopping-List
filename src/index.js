ea63fb77d4f6d2327a2935fb62c48296c9514f35
Zachary R Jackson <jackson.zachary.r@gmail.com>
2018-09-10 18:10:38 -0400

    Ability to add new items
    The ability to add new, nonexisting items has been added.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,98 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class List extends React.Component {
+  state = {
+    items: ["apple", "kiwi", "plum"],
+    newItemInput: "",
+    newItemValid: false
+  };
+
+  itemValid(item) {
+    /**
+     * Checks to see if an item is empty or in self.state.items
+     *
+     * :param item: String value of an item
+     * :return : Boolien True if not empty or in self.state.items
+     */
+    if (item === "") {
+      return false;
+    } else if (this.state.items.includes(item)) {
+      return false;
+    }
+    return true;
+  }
+
+  handleItemChange = event => {
+    /**
+     * Sets the state of newItemInput/newItemValid
+     *
+     * :event: takes a standard event value
+     */
+    this.setState({
+      newItemInput: event.target.value
+    });
+    const valid = this.itemValid(event.target.value);
+    this.setState({
+      newItemValid: valid
+    });
+  };
+
+  handleItemSubmit = event => {
+    /**
+     * Takes a form value and pushes it to this.state.items
+     * this.state.newItemInput is also reset
+     *
+     * :event: takes a standard event value
+     */
+    event.preventDefault();
+    const newItem = event.target.elements.newItem.value;
+    const newArrary = this.state.items.push(newItem);
+    this.setState({
+      screenDisplay: newArrary
+    });
+    this.setState({
+      newItemInput: ""
+    });
+  };
+
+  render() {
+    // Get all items out of this.state.items and turn them into paragraphs
+    let listItems = this.state.items.map((item, index) => {
+      const readableIndex = index + 1;
+      return (
+        <h1>
+          {readableIndex}. {item}
+        </h1>
+      );
+    });
+
+    return (
+      <div>
+        <h1>Shopping List</h1>
+        <div>{listItems}</div>
+
+        <form onSubmit={this.handleItemSubmit}>
+          <label>
+            New item:
+            <input
+              type="text"
+              value={this.state.newItemInput}
+              onChange={this.handleItemChange}
+              name="newItem"
+            />
+          </label>
+          <input
+            type="submit"
+            value="Submit"
+            disabled={!this.state.newItemValid}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const element = <List />;
+
+ReactDOM.render(element, document.getElementById("root"));
